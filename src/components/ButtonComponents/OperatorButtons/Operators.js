c8f83780e7f824afe1ee088890c6ab363da7d91b
taterntots <matthew.a.weidner@gmail.com>
2019-11-05 17:25:56 -0500

    Got the styling for the calculator pretty close to the design doc.

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -11,7 +11,7 @@ const Operators = () => {
   const [operator] = useState(operators);
 
   return (
-    <div className='operatorButtonSpacing'>
+    <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
