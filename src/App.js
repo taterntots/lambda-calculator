4c6148908dbf5968198b472e99b38df6680f66da
taterntots <matthew.a.weidner@gmail.com>
2019-11-05 15:49:29 -0500

    Got the Specials buttons to show up on the app. Not styled properly.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 // Don't forget to import any extra css/scss files you build into the correct component
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 import Display from './components/DisplayComponents/Display';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
@@ -20,6 +21,7 @@ function App() {
       <Logo />
       <div className="App">
         <Display />
+        <Specials />
         <Numbers />
         <Operators />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
+  // console.log(props);
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button>
+      {props.button}
+    </button>
   );
 };
+export default SpecialButton;
